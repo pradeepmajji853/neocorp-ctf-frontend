1f90c4d421376dd8c2c712b5b3863b17e97a72d9
Majji Pradeep Kumar <majjipradeepkumar@Majjis-MacBook-Air.local>
2026-04-13 18:34:13 +0530

    Deployment trigger: production hardening

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// Deployment Trigger: 2026-04-13 - Hardened Version
 import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Challenge1 from './pages/challenges/Challenge1'
